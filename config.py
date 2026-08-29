2485cb304b6f15a130f660405628aca6cd91b4be
Dhineshkumar-2007 <dhineshkumar.seat2024@dsuniversity.ac.in>
2026-08-29 17:10:21 +0530

    switched to cloud db

--- config.py
+++ config.py
@@ -11,25 +11,25 @@ class Config:
     # ===========================
     # LOCAL DATABASE - ACTIVE
     # ===========================
-
+    """
     DB_HOST = "localhost"
     DB_PORT = 3306
     DB_USER = "root"
     DB_PASSWORD = "Sql.dk2007@14"
     DB_NAME = "mydb1"
-
+    """
     # ===========================
     # CLOUD DATABASE - LATER
     # ===========================
 
-    """
+    
     DB_HOST = os.getenv("CLOUD_DB_HOST")
     DB_PORT = int(os.getenv("CLOUD_DB_PORT"))
     DB_USER = os.getenv("CLOUD_DB_USER")
     DB_PASSWORD = os.getenv("CLOUD_DB_PASSWORD")
     DB_NAME = os.getenv("CLOUD_DB_NAME")
     DB_SSL_CA = os.getenv("CLOUD_DB_SSL_CA")
-    """
+    
 
     SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = True
