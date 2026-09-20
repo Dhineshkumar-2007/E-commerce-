c1434e61ea99dd4d1e54c71c7014d50d12c75502
Dhinesh Kumar <dhineshkumarp.seat2024@dsuniversity.ac.in>
2026-09-20 15:13:08 +0530

    Format config.py with documentation quotes
    Added triple quotes for documentation and improved formatting.

--- config.py
+++ config.py
@@ -11,19 +11,19 @@ class Config:
     # ===========================
     # LOCAL DATABASE - ACTIVE
     # ===========================
-    
+    """
     DB_HOST = "localhost"
     DB_PORT = 3306
     DB_USER = "root"
     DB_PASSWORD = "Sql.dk2007@14"
     DB_NAME = "mydb1"
-    
+    """
     
     # ===========================
     # CLOUD DATABASE - LATER
     # ===========================
 
-    """
+    
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
     DB_USER = os.getenv("DB_USER")
@@ -32,7 +32,7 @@ class Config:
     DB_SSL_CA = os.getenv("DB_SSL_CA")
     
 
-    """
+    
 
     SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = True
